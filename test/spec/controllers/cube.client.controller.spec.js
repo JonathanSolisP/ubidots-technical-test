d606646616db6ffd8f4ea9408e52d8e4bf2378bd
Jonathan Solís Parajeles <jonathan.solis.parajeles@gmail.com>
2015-12-12 15:22:45 -0600

    Fix: Karma is now working. Add: Karma config file. Add: Some example test.

--- test/spec/controllers/cube.client.controller.spec.js
+++ test/spec/controllers/cube.client.controller.spec.js
@@ -1,24 +1,46 @@
-'use strict';
-
-describe("Controller: cubeCtrl", function() {
-    beforeEach(module('cubeSummationApp'));
-
-    var scope;
-    var cubeCtrl;
-    var utilityService;
-
-    beforeEach(inject(function ($controller, Utility, $rootScope) {
-        scope = $rootScope.$new();
-        utilityService = Utility;
-        cubeCtrl = $controller('cubeCtrl', {
-            $scope: scope,
-            Utility: utilityService
+describe("An AngularJS test suite", function () {
+    beforeEach(module('ngResource', function ($provide, $controllerProvider) {
+        $provide.value('fooVal', 5);
+        $controllerProvider.register('MainCtrl', function($scope){
+            $scope.hello = 'world';
+            this.reverse = function (input) {
+              return input.split('').reverse().join('');
+            };
         });
     }));
 
-    it("Create a 3D matrix with a specific length", function () {
-       expect(true).toBe(true);
-    });
+   it('should have tests', function () {
+      expect(true).toBe(true);
+   });
 
+    it('should inject dependencies', inject(function($resource, fooVal){
+        expect($resource).toBeDefined();
+        expect(fooVal).toBe(5);
+    }));
+
+    it('should compile angular expressions', inject(function($rootScope, $compile){
+        $rootScope.sum = 4;
+
+        var expression = '<p>2 + 2 == {{ sum }}</p>'
+        var element = $compile(expression)($rootScope);
+
+        expect(element.html()).not.toContain('2 + 2 == 4');
+
+        $rootScope.$digest();
+
+        expect(element.html()).toContain('2 + 2 == 4');
+    }));
+
+    it('should provide controllers', inject(function($rootScope, $controller){
+        var $scope = $rootScope.$new(),
+            ctrl = $controller('MainCtrl', { $scope: $scope});
+
+        expect($scope.hello).toBe('world');
+        expect(ctrl.reverse('foo')).toBe('oof');
+    }));
+});
+
+describe("Controller: cubeCtrl", function() {
+    beforeEach(module('cubeSummationApp'));
 
 });
